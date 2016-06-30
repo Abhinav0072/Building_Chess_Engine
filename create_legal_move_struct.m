0064ee3b49fb421039fade3c9cb0ba1ca5f0b112
Abhinav0072 <abhinavkumar.sieger@gmail.com>
2016-07-01 01:15:56 +0530

    Possible moves for "ROOK" completed.
    Possible moves for "ROOK" completed,so that it can be accessed by AI
    engine to get all possible moves for that piece(rook),according to
    rules.

--- create_legal_move_struct.m
+++ create_legal_move_struct.m
@@ -1,3 +1,3 @@
-function legal_move=create_legal_move_struct(current_state,final_file,final_rank,capturing_what)
-legal_move=struct('current_state',current_state,'final_file',final_file,'final_rank',final_rank,'capturing_what',capturing_what);
+function legal_move=create_legal_move_struct(current_status,final_file,final_rank,capturing_what)
+legal_move=struct('current_status',current_status,'final_file',final_file,'final_rank',final_rank,'capturing_what',capturing_what);
 end
